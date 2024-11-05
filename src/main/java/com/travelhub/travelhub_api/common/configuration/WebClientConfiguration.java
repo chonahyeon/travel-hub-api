beffd5cbc78d8114d0cdea31a393aa1a29112389
chonahyeon <whskgus12@nate.com>
2024-11-05 09:30:55 +0900

    * webclient 설정 반영

---
+++ src/main/java/com/travelhub/travelhub_api/common/configuration/WebClientConfiguration.java
@@ -0,0 +1,54 @@
+package com.travelhub.travelhub_api.common.configuration;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.LoggingCodecSupport;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@Slf4j
+public class WebClientConfiguration {
+
+    @Bean(name = "webClient")
+    public WebClient webClient() {
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024 * 20))
+                .build();
+
+        exchangeStrategies.messageWriters().stream()
+                .filter(LoggingCodecSupport.class::isInstance)
+                .forEach(writer -> ((LoggingCodecSupport) writer).setEnableLoggingRequestDetails(true));
+
+        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
+        uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE); // 인코딩 옵션 제거.
+
+        return WebClient.builder()
+                .uriBuilderFactory(uriBuilderFactory)
+                .clientConnector(new ReactorClientHttpConnector(
+                                HttpClient.create()
+                                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
+                                        .doOnConnected(conn -> conn.addHandler(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS)))
+                        )
+                )
+                .exchangeStrategies(exchangeStrategies)
+                .filter(ExchangeFilterFunction.ofRequestProcessor(
+                        clientRequest -> {
+                            log.debug("Request: {} {}", clientRequest.method(), clientRequest.url());
+                            return Mono.just(clientRequest);
+                        }
+                ))
+                .build();
+    }
+}
