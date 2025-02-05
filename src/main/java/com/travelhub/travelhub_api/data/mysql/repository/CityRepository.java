8d6743fb8201a39ae607f158ee55a1c7ad6bf0e6
chonahyeon <whskgus12@nate.com>
2025-02-05 09:25:04 +0900

    * feat: city 레파지토리 추가

---
+++ src/main/java/com/travelhub/travelhub_api/data/mysql/repository/CityRepository.java
@@ -0,0 +1,12 @@
+package com.travelhub.travelhub_api.data.mysql.repository;
+
+import com.travelhub.travelhub_api.data.mysql.entity.CityEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    Optional<CityEntity> findByCitName(String citName);
+}
